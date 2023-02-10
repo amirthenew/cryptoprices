b0cc4e8045256caf38a485d1b48b57138dcf0173
amirthenew <ah.abshari@hotmail.com>
2023-02-10 15:54:42 +0330

    styling tested module css

--- src/components/Coin.js
+++ src/components/Coin.js
@@ -1,11 +1,14 @@
 import React from 'react';
-
+import styles from './Coins.module.css'
 const Coin = ({name,image,symbol,price,marketCap,priceChange}) => {
 
-    return ( <div>
-        <img style={{width:'20px'}} src={image}/>
+    return ( <div className={styles.container}>
+        <img style={{width:'20px'}} src={image} alt='name'/>
         <span>{name}</span>
-        <span>{symbol}</span>
+        <span >{symbol}</span>
+        <span>{price}</span>
+        <span>{priceChange}</span>
+        <span>{marketCap}</span>
         
         </div> );
 }
