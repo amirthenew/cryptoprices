6dd77322d4bfde2b193610cc90068291dfbb7e60
amirthenew <ah.abshari@hotmail.com>
2023-02-18 23:39:54 +0330

    styled first one

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import GifLoader from 'react-gif-loader'
 import loadingGif from '../img/Loading_icon.gif'
+import styled from 'styled-components';
+
+const MyDiv = styled.div`
+background-color : #fff;
+
+
+`
 
 const Loader = () => {
-    return (<div>
+    return (<MyDiv>
         <GifLoader
         loading={true}
         imageSrc={loadingGif}
 
         />
-        </div>  );
+        </MyDiv>  );
 }
  
 export default Loader;
