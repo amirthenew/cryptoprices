fe0499be281d8b5f158402298006197fc446ff70
amirthenew <ah.abshari@hotmail.com>
2023-03-01 00:22:22 +0330

    Cta button added!

---
+++ src/components/Cta.js
@@ -0,0 +1,14 @@
+
+import React from 'react';
+import styles from './Cta.module.css'
+const Cta = () => {
+    return (
+
+        <div className={styles.container}>
+        <div>
+        </div>
+        </div>
+      );
+}
+ 
+export default Cta;
