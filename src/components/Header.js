152971810b909f843293e5622ed74abdaf5474ec
amirthenew <ah.abshari@hotmail.com>
2023-02-28 23:18:04 +0330

    styling on main div

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,30 +7,23 @@ const Header = (props) => {
 <div className={styles.maindiv}>
 <div className={styles.container}>
 <input className={styles.input} value={props.search} type='text' placeholder='search' onChange={props.searchHandler}/>
-
+<div className={styles.rows}>
 <div className={styles.name} onClick={props.sortByName} >
 Coin name
 </div>
-
 <div className={styles.symbol}>
 symbol
 </div>
-
 <div className={styles.price}>
 price
 </div>
-
 <div className={styles.change}>
 change
 </div>
-
-
 <div className={styles.mrketCap} onClick={props.mainSort}>
 Marketcap
+</div></div>
 </div>
-
-</div>
-
 </div>
 
      );
